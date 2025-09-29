124116c2ca2bcdfce2864679a6c5b18ba6e51763
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2025-09-29 20:01:47 +0530

    Over all Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Pages/Home/Home";
-import './App.css'
 import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
 import AboutUs from "./Pages/AboutUs/AboutUs";
 import Services from "./Pages/Services/Services";
 import OurTeam from "./Pages/OurTeam/OurTeam";
 import TeamDetails from "./Pages/TeamDetails/TeamDetails";
-
+import "./App.css";
+import PageWrapper from "./Components/PageWrapper";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/team" element={<OurTeam />} />
-        <Route path="/team/details" element={<TeamDetails />} />
-      </Routes>
-      <Footer/>
-    </Router>
+      <PageWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/team" element={<OurTeam />} />
+          <Route path="/team/details" element={<TeamDetails />} />
+        </Routes>
+      </PageWrapper>
+      <Footer />
+    </>
   );
 }
 
